94dc9d15303f213ade93dd6ac6e754d09ead8bfd
ktseo41 <ktseo41@gmail.com>
2019-12-24 19:38:58 +0900

    add: basic script

---
+++ trePuppet.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer');
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const bot = new TelegramBot(process.env.botToken);
+const interval = 20 * 1000;
+const timerFlag = 'needStart';
+const reloadMax = 100;
+
+let counter = 0;
+let timerId = timerFlag;
+
+const start = async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(process.env.ssEssay45Url);
+  if (timerId === timerFlag)
+    timerId = setInterval(() => scrap(page, browser), interval);
+};
+
+const scrap = async (page, browser) => {
+  await page.content();
+
+  const isFull = await page.$x('//button[text()="클럽 둘러보러 가기"]');
+
+  if (!isFull) {
+    bot.sendMessage(process.env.channelChatId, '빈 자리가 생겼어요!~');
+    clearInterval(timerId);
+  }
+
+  if (isFull) {
+    counter += 1;
+
+    await page.reload();
+
+    if (counter % reloadMax === 0) {
+      clearInterval(timerId);
+      bot.sendMessage(
+        process.env.channelChatId,
+        `${counter}회 실행 완료 재시작합니다.`
+      );
+      timerId = timerFlag;
+      await browser.close();
+      start();
+    }
+
+    return;
+  }
+
+  await browser.close();
+};
+
+start();
